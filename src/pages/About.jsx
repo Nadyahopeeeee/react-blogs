003a2b99afb98104887fde487c8de881945cf273
Семененко Надежда <tn@impsa.ru>
2023-04-21 17:02:57 +0300

    adding posts from server

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const About = () => {
-  return <div>Это приложение создано</div>;
+  return (
+    <div>
+      <h2>Страница о приложении на стадии разработки.</h2>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useFetching } from '../hooks/useFetching';
+import PostService from '../API/PostService';
+import Loader from '../components/UI/Loader/Loader';
 
 const PostIdPage = () => {
+  const params = useParams();
+  const [post, setPost] = useState({});
+  const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+    const response = await PostService.getById(params.id);
+    setPost(response.data);
+  });
+
+  useEffect(() => {
+    fetchPostById(params.id);
+  }, []);
+
   return (
     <div>
-      <h2>страница поста</h2>
+      <h2>Вы открыли страинцу поста с ID = {params.id}</h2>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          {post.id}. {post.title}
+        </div>
+      )}
     </div>
   );
 };
